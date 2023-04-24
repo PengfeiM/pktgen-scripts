4233a67dd0e24d6668ad130d7b8a65415d069e40
马朋飞 <mapengfei.0706@bytedance.com>
2023-04-24 17:41:26 +0800

    add total pps

--- print_results.sh
+++ print_results.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+echo "===== pktgen result summary ====="
+
+let total_pps=0
+
 for ((i = 0; i < $1; i++)); do
         echo "Device: eth0@${i}"
-        cat /proc/net/pktgen/eth0@${i} | grep -A2 "Result:"
+        result_cur_device=$(cat /proc/net/pktgen/eth0@${i} | grep -A2 "Result:")
+        echo $result_cur_device
+        cur_device_pps=$(echo $result_cur_device | awk '{print $7}')
+        cur_pps_num=$((${cur_device_pps%pps}))
+        total_pps=$(($total_pps+$cur_pps_num))
 	echo "--------"
 done
+
+echo "total_pps: $total_pps"
+
+echo "===== end ====="
